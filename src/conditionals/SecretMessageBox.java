acbecb9484082a443024b76715fa47685cc8bd9b
Asher Tedford <league@WorthyMini-6.attlocal.net>
2017-10-08 15:58:58 -0700

    SecretMessageBox, BananaQuiz

--- src/conditionals/SecretMessageBox.java
+++ src/conditionals/SecretMessageBox.java
@@ -1,33 +1,24 @@
 //    Copyright (c) The League of Amazing Programmers 2013-2017
-//    Level 0
-
 package conditionals;
 
 import javax.swing.JOptionPane;
 
-/**
- * Secret Message Box / Secure Messaging System
- * 
- * You want to leave a message on one of the Mac computers so that only your friend can read it.
- * You set up the passcode and the secret message. 
- * Your friend types in the passcode to retrieve the message.
- * 
- */
-
 public class SecretMessageBox {
+	public static void main(String[] args) {
 
-	// 0. Make a main method and put steps 1-5 inside it
-	
-	// 1. Set the passcode in a String variable
-	
-	// 2. Using a pop-up, ask for a secret message and store it in a variable
+		String username = "Grand Moff Tarkin";
+		String passcode = "Zoog";
 
-	// 3. Ask your friend for the passcode and store it in a variable
-
-	// 4. If the passcode matches, show the secret message
-
-	// 5. If the passcode does not match, pop-up "INTRUDER!!"
-
-	// [optional] 6. Have your friend also enter a username, and make sure it is correct before releasing the secret message.
-
+		boolean match = JOptionPane.showInputDialog("Who is this?").equalsIgnoreCase(username);
+		if (match) {
+			boolean match1 = JOptionPane.showInputDialog("What is the passcode?").equalsIgnoreCase(passcode);
+			if (match1) {
+				JOptionPane.showMessageDialog(null, "You have been granted the plans to the Death Star.");
+			} else {
+				JOptionPane.showMessageDialog(null, "INRUDER!!!");
+			}
+		} else {
+			JOptionPane.showMessageDialog(null, "INTRUDER!!!");
+		}
+	}
 }
